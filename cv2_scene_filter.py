39f8338b9dfb65bc65e94de8ebb8f5764ab2033b
tsiganskytabor <makskirich03@mail.ru>
2024-05-16 19:56:34 +0300

    Separation execution time into decode time and recognition time

--- cv2_scene_filter.py
+++ cv2_scene_filter.py
@@ -14,36 +14,53 @@ def detect_scene_changes_cv2(video_file):
     ret, prev_frame = cap.read()
     if not ret:
         print("Error: Could not read the video file.")
-        return []
+        return [], 0, 0, 0, 0
+
     prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
     scene_changes = []
 
-    start_time = time.time()
+    start_time_total = time.time()
+    start_time_decode = time.time()
+    decode_time = 0
+    recognition_time = 0
     cpu_usage_before = psutil.cpu_percent(interval=None)
     while True:
         ret, frame = cap.read()
         if not ret:
             break
+        end_time_decode = time.time()
+        decode_time += end_time_decode - start_time_decode
+
+        start_time_recognition = time.time()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if detect_scene_change(prev_gray, gray):
             scene_changes.append(cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0)
         prev_gray = gray
+        end_time_recognition = time.time()
+        recognition_time += end_time_recognition - start_time_recognition
+
+        start_time_decode = time.time()
+
     cap.release()
-    end_time = time.time()
+    end_time_total = time.time()
     cpu_usage_after = psutil.cpu_percent(interval=None)
-    fps = len(scene_changes) / (end_time - start_time)
+    execution_time = end_time_total - start_time_total
+    decode_time += time.time() - start_time_decode  # Добавляем оставшееся время декодирования после выхода из цикла
+    fps = len(scene_changes) / execution_time
     memory = psutil.Process().memory_info().rss / 1024 / 1024
-    return scene_changes, end_time - start_time, memory, cpu_usage_after - cpu_usage_before, fps
+    return scene_changes, execution_time, decode_time, recognition_time, memory, cpu_usage_after - cpu_usage_before, fps
 
 
 if __name__ == "__main__":
     video_path = "four_cars_night.mp4"
-    scene_changes, execution_time, memory_usage, cpu_usage, fps = detect_scene_changes_cv2(video_path)
+    scene_changes, execution_time, decode_time, recognition_time, memory_usage, cpu_usage, fps = detect_scene_changes_cv2(video_path)
     print("Scene Changes:")
     for scene_num, scene_time in enumerate(scene_changes):
         print(f"Scene {scene_num}:", scene_time)
     print("\n--- cv2_scene_filter ---")
     print("Execution Time:", execution_time, "seconds")
+    print("Decode Time:", decode_time, "seconds")
+    print("Recognition Time:", recognition_time, "seconds")
     print("FPS:", fps)
     print("Memory Usage:", memory_usage, "MB")
     print("CPU Usage:", cpu_usage, "%")
--- ffmpeg_scene_filter.py
+++ ffmpeg_scene_filter.py
@@ -12,30 +12,45 @@ def detect_scene_changes(path):
         .filter('showinfo')
         .output('dummy.mp4', format='null', codec='libx264')
     )
-    start_time = time.time()
+    start_time_total = time.time()
     cpu_usage_before = psutil.cpu_percent(interval=None)
+
+    start_time_decode = time.time()
     stdout, stderr = ffmpeg.run(output_stream, capture_stdout=True, capture_stderr=True)
-    end_time = time.time()
-    cpu_usage_after = psutil.cpu_percent(interval=None)
+    end_time_decode = time.time()
+    decode_time = end_time_decode - start_time_decode
+
+    start_time_recognition = time.time()
     output = stderr.decode('utf-8')
     scene_changes = []
     for line in output.split('\n'):
         match = re.search(r'pts_time:([\d.]+)', line)
         if match:
             scene_changes.append(float(match.group(1)))
-    fps = len(scene_changes) / (end_time - start_time)
+    end_time_recognition = time.time()
+    recognition_time = end_time_recognition - start_time_recognition
+
+    end_time_total = time.time()
+    cpu_usage_after = psutil.cpu_percent(interval=None)
+
+    execution_time = end_time_total - start_time_total
+    fps = len(scene_changes) / execution_time
     memory = psutil.Process().memory_info().rss / 1024 / 1024
-    return scene_changes, end_time - start_time, memory, cpu_usage_after - cpu_usage_before, fps
+
+    return scene_changes, execution_time, decode_time, recognition_time, memory, cpu_usage_after - cpu_usage_before, fps
 
 
 if __name__ == "__main__":
     video_path = 'four_cars_night.mp4'
-    changes, execution_time, memory_usage, cpu_usage, fps = detect_scene_changes(video_path)
+    changes, execution_time, decode_time, recognition_time, memory_usage, cpu_usage, fps = detect_scene_changes(
+        video_path)
     print("Scene Changes:")
     for scene_num in range(len(changes)):
         print(f"Scene {scene_num}:", changes[scene_num])
     print("\n--- ffmpeg_scene_filter ---")
     print("Execution Time:", execution_time, "seconds")
+    print("Decode Time:", decode_time, "seconds")
+    print("Recognition Time:", recognition_time, "seconds")
     print("FPS:", fps)
     print("Memory Usage:", memory_usage, "MB")
     print("CPU Usage:", cpu_usage, "%")
